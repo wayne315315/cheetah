1efa3ae7ec3ee246349246ff34ac656cefb460ff
linhsuanyu <wayne315315@gmail.com>
2019-10-20 10:52:08 +0800

    Complete crawler

---
+++ config.py
@@ -0,0 +1,2 @@
+# Daily Stock Price and Volume in TWSE (fill datestring in %s)
+TAIWAN_URL = "https://www.twse.com.tw/en/exchangeReport/MI_INDEX?response=csv&date=%s&type=ALL"
---
+++ crawl.py
@@ -0,0 +1,44 @@
+from io import StringIO
+
+import requests
+import pandas as pd
+
+from config import TAIWAN_URL
+
+
+def crawl(datestring):
+	url = TAIWAN_URL % datestring
+
+	rawdata = requests.post(url).text
+	header, footer = [], []
+	for i, line in enumerate(rawdata.split("\n")):
+	    if "Security Code" in line:
+	        header.append(i)
+	    if "Remarks:" in line:
+	        footer.append(i)
+	header, footer = header[0], footer[-1]
+	rawdata = "\n".join(rawdata.replace("=", "").split("\n")[header:footer])
+
+	usecols = ['Security Code',
+	           'Trade Volume', 
+	           'Transaction', 
+	           'Trade Value', 
+	           'Opening Price',
+	           'Highest Price', 
+	           'Lowest Price', 
+	           'Closing Price', 
+	           'Dir(+/-)', 
+	           'Change',
+	           'Last Best Bid Price', 
+	           'Last Best Bid Volume', 
+	           'Last Best Ask Price',
+	           'Last Best Ask Volume', 
+	           'Price-Earning ratio']
+
+	df = pd.read_csv(StringIO(rawdata), usecols = usecols, index_col="Security Code")
+	df = df.apply(lambda s: pd.to_numeric(s.astype(str).str.replace(",", "").replace("+", "1").replace("-", "-1"), errors='coerce'))
+	df = df.fillna(value={"Dir(+/-)":0})
+	df["Change"] *= df["Dir(+/-)"]
+	df = df.drop(columns="Dir(+/-)")
+
+	return df
